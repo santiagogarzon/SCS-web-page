fecb8f03316ec9b1844b0a409f7963db910ed416
Santi Garzon <santiagogarzon04@gmail.com>
2019-01-14 09:25:09 -0300

    fixing bug

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+	/*
+	Dialog with CSS animation
+	*/
+	$('.popup-with-zoom-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
